1873de59f04c76c7effd89fe379f1509aca66a1d
Hector Romero <hromero.mx@gmail.com>
2021-06-30 05:00:38 -0700

    Basic test page

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello Smart Table
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
