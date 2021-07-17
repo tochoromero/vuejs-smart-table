4d668f104248b710b73ca64d4b3fcc53cffa278b
Hector Romero <hromero.mx@gmail.com>
2021-07-17 07:27:09 -0700

    Fix sort icons

--- src/icon-utils.ts
+++ src/icon-utils.ts
@@ -36,21 +36,15 @@ export function createSortIcon(order: SortOrder) {
     viewBox: `0 0 320 512`
   }
 
-  const pathAttrs = (d: string, opacity: number) => ({
-    fill: 'currentColor',
-    opacity,
-    d
-  })
-
   const upAttrs = () => ({
     fill: 'currentColor',
-    opacity: order === SortOrder.ASC ? 1 : 0.4,
+    opacity: order === SortOrder.NONE ? 0.4 : (order === SortOrder.ASC ? 0.4 : 1),
     d: 'M41.05 288.05h238c21.4 0 32.1 25.9 17 41l-119 119a23.9 23.9 0 0 1-33.8.1l-.1-.1-119.1-119c-15.05-15.05-4.4-41 17-41z'
   })
 
   const downAttrs = () => ({
     fill: 'currentColor',
-    opacity: order === SortOrder.DESC ? 1 : 0.4,
+    opacity: order === SortOrder.NONE ? 0.4 : (order === SortOrder.DESC ? 0.4 : 1),
     d: 'M24.05 183.05l119.1-119A23.9 23.9 0 0 1 177 64a.94.94 0 0 1 .1.1l119 119c15.1 15.1 4.4 41-17 41h-238c-21.45-.05-32.1-25.95-17.05-41.05z'
   })
 
